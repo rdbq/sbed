594f1d0235d97a2c0939841dbb0819dfabd5b71e
rdbq <906913330@qq.com>
2018-10-05 20:43:36 +0800

    create brache dev

--- src/main/java/io/sbed/api/HomeController.java
+++ src/main/java/io/sbed/api/HomeController.java
@@ -3,5 +3,6 @@ package io.sbed.api;
 public class HomeController {
     public static void main(String[] args) {
         System.out.println("create brache dev....");
+        System.out.println("sdfsd");
     }
 }
